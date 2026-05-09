2bd6015193bed7a616f1373ac394f9366f2d029a
Sanity <pheobyte18@yahoo.com>
2026-05-09 21:31:05 +0300

    Add production deploy workflow and update baseUrl

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "pheobyte.github.io/obsidian-hobby-project-dnd",
     ignorePatterns: [
       // Hide all system/utility folders
       "__INTEGRITY_CHECKS",
